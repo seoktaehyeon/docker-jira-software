24616eb81486e915e8f3ccd9a499e3d41e77673e
seoktaehyeon <seoktaehyeon@msn.com>
2021-04-01 19:25:50 +0800

    improve setMem.sh

--- tool/setMem.sh
+++ tool/setMem.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
-#mem_total=$(free -m | grep Mem | awk '{print $2}')
-#echo -e "Mem Total: ${mem_total}m"
-#jvm_mem_max=$(( mem_total / 100 * 80 ))
+mem_total=$(free -m | grep Mem | awk '{print $2}')
+echo -e "Mem Total: ${mem_total}m"
 mem_limit=$(( $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes) / 1024 / 1024 ))
 echo -e "Mem Limit: ${mem_limit}m"
-jvm_mem_max=$(( mem_limit / 100 * 80 ))
+
+if (( mem_limit < mem_total )); then
+    jvm_mem_max=$(( mem_limit / 100 * 80 ))
+else
+    jvm_mem_max=$(( mem_total / 100 * 80 ))
+fi
 echo -e "Suggest JVM Mem Max: ${jvm_mem_max}m"
+
 if (( jvm_mem_max > 8196 )); then
     jvm_mem_min=4096
 else
@@ -21,4 +26,3 @@ echo -e "Done"
 echo -en "Set JVM_MAXIMUM_MEMORY ... "
 sed -i "s/JVM_MAXIMUM_MEMORY:=.*/JVM_MAXIMUM_MEMORY:=${jvm_mem_max}m}/" ${setenv_file}
 echo -e "Done"
-
